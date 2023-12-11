113a2cc7015ea7b952cd2e6fc2e44560fb2dc3c1
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2023-12-11 21:57:12 +0100

    Completed 2.7 class (set up wait() and page elements)

---
+++ src/main/java/com/example/teamcity/ui/pages/favorites/ProjectsPage.java
@@ -0,0 +1,36 @@
+package com.example.teamcity.ui.pages.favorites;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.selector.ByAttribute;
+import com.example.teamcity.ui.Selectors;
+import com.example.teamcity.ui.elements.ProjectElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.elements;
+
+public class ProjectsPage extends FavoritesPage {
+    public static final String FAVORITE_PROJECTS_URL = "/favorite/projects";
+
+    // we have got a list of elements from Selenide collection
+    private ElementsCollection subprojects = elements(Selectors.byClass("ProjectPageHeader__title__ih"));
+
+    // but we want to get List<ProjectElement>. So we need to get data from <ElementCollection> to List<ProjectElement>
+    public ProjectsPage open() {
+        Selenide.open(FAVORITE_PROJECTS_URL);
+        waitUntilFavoritePageIsLoaded();
+        // we return the same page, that`s why return this
+        return this;
+    }
+
+    public List<ProjectElement> getSubprojects() {
+        return generatePageElements(subprojects, ProjectElement::new);
+        // ProjectElement::new - we invoked our constructor, and used it while creating a new element in Page class (where we used <T>
+    }
+
+
+
+
+}
