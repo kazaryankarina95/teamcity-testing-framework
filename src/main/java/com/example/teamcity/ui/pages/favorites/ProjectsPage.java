858de47f6cb7e627560bcd81c4a7c75cfe44f1b3
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-01-25 22:12:45 +0100

    cleaned up imports

--- src/main/java/com/example/teamcity/ui/pages/favorites/ProjectsPage.java
+++ src/main/java/com/example/teamcity/ui/pages/favorites/ProjectsPage.java
@@ -2,11 +2,9 @@ package com.example.teamcity.ui.pages.favorites;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.selector.ByAttribute;
 import com.example.teamcity.ui.Selectors;
 import com.example.teamcity.ui.elements.ProjectElement;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.codeborne.selenide.Selenide.elements;
