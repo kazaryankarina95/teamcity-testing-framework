4564e34f581c189ccc80eb398d50e61118bd41c3
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-01-25 22:12:05 +0100

    cleaned up imports

--- src/main/java/com/example/teamcity/ui/pages/LoginPage.java
+++ src/main/java/com/example/teamcity/ui/pages/LoginPage.java
@@ -2,7 +2,6 @@ package com.example.teamcity.ui.pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.selector.ByAttribute;
 import com.example.teamcity.api.models.User;
 import com.example.teamcity.ui.Selectors;
 import lombok.Getter;
