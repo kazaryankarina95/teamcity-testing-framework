65301f7574be8ae930a950d1d7ad43cda7963fed
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-01-25 22:13:37 +0100

    Created StartUpPage and SetupTest

---
+++ src/main/java/com/example/teamcity/ui/pages/admin/StartUpPage.java
@@ -0,0 +1,41 @@
+package com.example.teamcity.ui.pages.admin;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.example.teamcity.ui.Selectors;
+import com.example.teamcity.ui.pages.Page;
+import lombok.Getter;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.element;
+
+public class StartUpPage extends Page {
+
+    @Getter
+    private SelenideElement header = element(Selectors.byId("header"));
+    private SelenideElement proceedButton = element("input[id='proceedButton']");
+    private SelenideElement restoreButton = element("input[id='restoreButton']");
+    private SelenideElement acceptLicense = element("input[id='accept']");
+    private SelenideElement backupFileUploaded = element("input[id='backup']");
+
+    public StartUpPage open() {
+        Selenide.open("/");
+        return this;
+    }
+
+    public StartUpPage setupTeamCityServer() {
+        waitUntilPageIsLoaded();
+        proceedButton.click();
+        waitUntilPageIsLoaded();
+        proceedButton.click();
+        waitUntilPageIsLoaded();
+        acceptLicense.shouldBe(Condition.enabled, Duration.ofMinutes(3));
+        acceptLicense.scrollTo();
+        acceptLicense.click();
+        submit();
+        return this;
+    }
+}
+
---
+++ src/test/java/com/example/teamcity/ui/SetupTest.java
@@ -0,0 +1,16 @@
+package com.example.teamcity.ui;
+
+import com.codeborne.selenide.Condition;
+import com.example.teamcity.ui.pages.admin.StartUpPage;
+import org.testng.annotations.Test;
+
+public class SetupTest extends BaseUiTest{
+
+    @Test
+    public void startUpTest() {
+        new StartUpPage()
+                .open()
+                .setupTeamCityServer()
+                .getHeader().shouldHave(Condition.text("Create Administrator account"));
+    }
+}
