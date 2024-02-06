32cfcbe74940e6955517a438e9a82c817d8b03af
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-02-06 21:33:00 +0100

    Renamed startUpTest to startUpServerTest in SetupTest

--- src/test/java/com/example/teamcity/ui/SetupTest.java
+++ src/test/java/com/example/teamcity/ui/SetupTest.java
@@ -4,10 +4,11 @@ import com.codeborne.selenide.Condition;
 import com.example.teamcity.ui.pages.admin.StartUpPage;
 import org.testng.annotations.Test;
 
-public class SetupTest extends BaseUiTest{
+public class SetupTest extends BaseUiTest {
+
 
     @Test
-    public void startUpTest() {
+    public void startUpServerTest() {
         new StartUpPage()
                 .open()
                 .setupTeamCityServer()
