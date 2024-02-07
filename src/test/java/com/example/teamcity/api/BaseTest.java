5950dc4cb5f139258012f2b575d6ba81e56605ad
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-02-07 12:28:46 +0100

    Added @BeforeEach in BaseTest

--- src/test/java/com/example/teamcity/api/BaseTest.java
+++ src/test/java/com/example/teamcity/api/BaseTest.java
@@ -1,10 +1,18 @@
 package com.example.teamcity.api;
 
+import com.codeborne.selenide.Configuration;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class    BaseTest {
+    @BeforeEach
+    public void setUp() {
+        Configuration.headless = true;
+        Configuration.browser = "firefox";
+    }
+
     protected SoftAssertions softy;
 
     @BeforeMethod
