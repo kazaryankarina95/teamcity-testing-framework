9e3b8e2b040f0c2534386e8bd4e0ebe24bfe42ad
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-02-08 11:35:27 +0100

    modified waiting to 30000 mls

--- src/test/java/com/example/teamcity/StartUpAgentTest.java
+++ src/test/java/com/example/teamcity/StartUpAgentTest.java
@@ -14,36 +14,22 @@ public class StartUpAgentTest extends BaseApiTest {
     @Tag("SetupTest")
     public void authorizeAgentTest() {
         var requestedAgent = new AgentRequests(Specifications.getSpec().superUserSpec());
-        int retries = 10; // Max number of attempts
-        for (int i = 0; i < retries; i++) {
-            AgentList listOfAgents = requestedAgent.get();
-
-            // If list is not empty, break the loop
-            if (!listOfAgents.getAgent().isEmpty()) {
-                break;
-            }
-
-            // If list is empty, wait for a while before next try
-            try {
-                Thread.sleep(10000); // Sleep for 500 milliseconds before next try
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            // If this is the last iteration and list is still empty, handle the error appropriately
-            if (i == retries - 1) {
-                // Handle error
-            }
-
-            System.out.println(listOfAgents);
-
-            Agent defaultAgent = listOfAgents.getAgent().get(0);
-            var defaultAgentName = defaultAgent.getName();
-            System.out.println(defaultAgentName);
-
-            requestedAgent.put(defaultAgentName)
-                    .then().assertThat().statusCode(HttpStatus.SC_OK)
-                    .extract().asString();
+        try {
+            // wait for 5 seconds
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            // handle the exception
+            e.printStackTrace();
         }
+        AgentList listOfAgents = requestedAgent.get();
+        System.out.println(listOfAgents);
+
+        Agent defaultAgent = listOfAgents.getAgent().get(0);
+        var defaultAgentName = defaultAgent.getName();
+        System.out.println(defaultAgentName);
+
+        requestedAgent.put(defaultAgentName)
+        .then().assertThat().statusCode(HttpStatus.SC_OK)
+                .extract().asString();
     }
 }
