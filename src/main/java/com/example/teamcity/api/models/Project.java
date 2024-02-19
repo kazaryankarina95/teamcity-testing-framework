7603a4b1fb469f23a1c8cd43a86ca6ca2090b6d5
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-02-19 18:01:40 +0100

    Added @JsonIgnoreProperties to Project model

--- src/main/java/com/example/teamcity/api/models/Project.java
+++ src/main/java/com/example/teamcity/api/models/Project.java
@@ -1,5 +1,6 @@
 package com.example.teamcity.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"internalId", "uuid", "parentProjectInternalId", "parentProjectName", "archived", "virtual", "description", "href", "webUrl", "links", "parentProject", "readOnlyUI", "defaultTemplate", "buildTypes", "templates", "parameters", "vcsRoots", "projectFeatures", "projects", "cloudProfiles", "ancestorProjects"})
 // this is what we get as an output when creating a new project
 public class Project {
     // Get project`s field from https://www.jetbrains.com/help/teamcity/rest/project.html
