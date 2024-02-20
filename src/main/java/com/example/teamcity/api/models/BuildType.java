31a84e787d58fb81583fe57f18c4b77d12584058
Karina.Kazaryan <karina.kazaryan@jetbrains.com>
2024-02-20 12:08:25 +0100

    Added @JsonIgnoreProperties annotation into BuildType class

--- src/main/java/com/example/teamcity/api/models/BuildType.java
+++ src/main/java/com/example/teamcity/api/models/BuildType.java
@@ -1,5 +1,6 @@
 package com.example.teamcity.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"projectName", "projectId", "href", "webUrl", "buildTypeId", "parentProjectId", "templates", "vcs-root-entries", ""})
 public class BuildType {
+    // https://www.jetbrains.com/help/teamcity/rest/buildtype.html
     private String id;
     private NewProjectDescription project;
     private String name;
